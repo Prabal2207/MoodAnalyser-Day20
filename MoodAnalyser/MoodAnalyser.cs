f1797a2c388239fbc47f4cc04ad2293ba162756b
Prabal2207 <prabal760@gmail.com>
2021-07-28 22:31:29 +0530

    [Prabal]Added:-Given message,ability to analyse sad or happy mood

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace MoodAnalyser1
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        //public MoodAnalyser(string message)
+        //{
+        //    this.message = message;
+        //}
+        //public MoodAnalyser()
+        //{
+        //}
+
+        public string analyseMood(string message)
+        {
+            //"null"==""
+            // string s = null;
+
+            if (message.Contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+
+
+        }
+    }
+
+
+
+}
---
+++ MoodTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser1;
+
+
+namespace MoodTest
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMoodHappy_ShouldReturnHappy()
+        {
+            MoodAnalyser obj = new MoodAnalyser();
+            string result = obj.analyseMood("HAPPY");
+            Assert.AreEqual("HAPPY", result);
+        }
+
+        [TestMethod]
+        public void GivenMoodSad_ShouldReturnSad()
+        {
+            MoodAnalyser obj = new MoodAnalyser();
+            string result = obj.analyseMood("SAD");
+            Assert.AreEqual("SAD", result);
+        }
+
+
+    }
+
+
+
+}
